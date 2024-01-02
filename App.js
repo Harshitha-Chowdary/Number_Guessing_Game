387d10a0e1ce204e255597858ff3fe1c98f1de79
Harshitha-Chowdary <harshithachowdary456@gmail.com>
2024-01-02 23:09:41 +0530

    SecondApp_firstCommit

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./Screens/StartGameScreen";
+
+export default function App(){
+  return(
+    <StartGameScreen/>
+  )
+}
+const style=StyleSheet.create({});
+
+
